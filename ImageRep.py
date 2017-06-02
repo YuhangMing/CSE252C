93857b5cd873a8c3fa0da8da6029cbebfcf345e7
Xiaofei Xian <wangjinglalala@gmail.com>
2017-06-02 16:51:06 -0700

    cols,rows -> shape

--- ImageRep.py
+++ ImageRep.py
@@ -13,16 +13,16 @@ kNumBins = 16;
 class ImageRep:
     def __init__(self, image, computeIntegral, computeIntegralHists, colour):
         self.m_channels = 3 if colour else 1
-        self.m_rect = Rect(0, 0, image.cols, image.rows)
+        self.m_rect = Rect(0, 0, image.shape[1], image.shape[0])
         self.m_images, self.m_integralImages, self.m_integralHistImages = [],[],[]
         
         for i in range(self.m_channels):
-            self.m_images.append(cv.createMat(image.rows, image.cols, cv.CV_8UC1))
+            self.m_images.append(cv.createMat(image.shape[0], image.shape[1], cv.CV_8UC1))
             if computeIntegral:
-                self.m_integralImages.append(cv.createMat(image.rows+1, image.cols+1, cv.CV_32SC1))
+                self.m_integralImages.append(cv.createMat(image.shape[0]+1, image.shape[1]+1, cv.CV_32SC1))
             if computeIntegralHist:
                 for j in range(kNumBins):
-                    self.m_integralHistImages.append(cv.createMat(image.rows+1, image.cols+1, cv.CV_32SC1))
+                    self.m_integralHistImages.append(cv.createMat(image.shape[0]+1, image.shape[1]+1, cv.CV_32SC1))
         
         if colour:
             assert(image.channels() == 3)
@@ -41,22 +41,22 @@ class ImageRep:
                 self.m_integralImages[i] = cv.integral(self.m_images[i])
         
         if computeIntegralHist:
-            tmp = cv.createMat(image.rows, image.cols, cv.CV_8UC1)
+            tmp = cv.createMat(image.shape[0], image.shape[1], cv.CV_8UC1)
             tmp[:] = 0
             for j in range(kNumBins):
-                for y in range(image.rows):
-                    for x in range(image.cols):
+                for y in range(image.shape[0]):
+                    for x in range(image.shape[1]):
                         sbin = int(float(self.m_images[0][y][x])/256.0*kNumBins)
                         tmp[y][x] = 1 if sbin == j else 0
                 self.m_integralImages[j] = cv.integral(tmp)
    
     
     def Sum(self, rRect, channel=0):
-        assert(rRect.XMin()>=0 and rRect.YMin()>=0 and                rRect.XMax()<=self.m_images[0].cols and rRect.YMax()<=self.m_images[0].rows)
+        assert(rRect.XMin()>=0 and rRect.YMin()>=0 and                rRect.XMax()<=self.m_images[0].shape[1] and rRect.YMax()<=self.m_images[0].shape[0])
         return self.m_integralImages[channel][rRect.YMin()][rRect.XMin()] +                 self.m_integralImages[channel][rRect.YMax()][rRect.XMax()] -                 self.m_integralImages[channel][rRect.YMax()][rRect.XMin()] -                 self.m_integralImages[channel][rRect.YMin()][rRect.XMax()]
     
     def Hist(self, rRect, h):
-        assert(rRect.XMin()>=0 and rRect.YMin()>=0 and                 rRect.XMax()<=self.m_images[0].cols and rRect.YMax()<=slef.m_images[0].rows)
+        assert(rRect.XMin()>=0 and rRect.YMin()>=0 and                 rRect.XMax()<=self.m_images[0].shape[1] and rRect.YMax()<=slef.m_images[0].shape[0])
         norm = rRect.Area();
         for i in range(kNumBins):
             total = self.m_integralHistImages[i][rRect.YMin()][rRect.XMin()] +                     self.m_integralHistImages[i][rRect.YMax()][rRect.XMax()] -                     self.m_integralHistImages[i][rRect.YMax()][rRect.XMin()] -                     self.m_integralHistImages[i][rRect.YMin()][rRect.XMax()]
