becd93ad41b1e70049021dec5f6178b050133f65
YuhangMing <Yohann@YuhangdeMacBook-Pro.local>
2017-06-03 16:36:57 -0700

    reduce called functions

--- HaarFeatures.py
+++ HaarFeatures.py
@@ -22,8 +22,8 @@ class HaarFeatures:
 
 	def add_feature(self, new_feature):
 		self.m_features.append(new_feature)
-	def add_featVec(self, new_featVec):
-		self.m_featVec.append(new_featVec)
+	# def add_featVec(self, new_featVec):
+	# 	self.m_featVec.append(new_featVec)
 
 	def GetCount(self):
 		return self.m_featureCount
@@ -42,31 +42,31 @@ class HaarFeatures:
 						new_feature = HaarFeature(r, itype)
 						self.add_feature(new_feature)
 
-	def UpdateFeatureVector(self, sample):
-		self.m_featVec = []
-		for i in range(self.m_featureCount):
-			new_featVec = self.m_features[i].Eval(sample)
-			self.m_featVec.append(new_featVec)
-			# self.add_featVec(new_featVec)
-		# print('length of m_featVec: '+str(len(self.m_featVec)))
-		return self.m_featVec
+	# def UpdateFeatureVector(self, sample):
+	# 	self.m_featVec = []
+	# 	for i in range(self.m_featureCount):
+	# 		new_featVec = self.m_features[i].Eval(sample)
+	# 		self.m_featVec.append(new_featVec)
+	# 		# self.add_featVec(new_featVec)
+	# 	# print('length of m_featVec: '+str(len(self.m_featVec)))
+	# 	return self.m_featVec
 
 
-	def EvalOne(self, s):
-		return self.UpdateFeatureVector(s)
+	# def EvalOne(self, s):
+	# 	return self.UpdateFeatureVector(s)
 
 
 	def Eval(self, s, featVec):
 		# print('# of rects: ' + str(len(s.GetRects())))
-		for i in range(len(s.GetRects())):
-			# print('inside Eval: '+ self.EvalOne(s.GetSample(i)))
-			# featVec[i].append(self.EvalOne(s.GetSample(i)))
-			featVec[i, :] = self.EvalOne(s.GetSample(i))
-		# print(len(featVec))
-		# print(featVec[1])
+		for j in range(len(s.GetRects())):
+			# featVec[i, :] = self.UpdateFeatureVector(s.GetSample(i))
+			# featVec[i, :] = self.EvalOne(s.GetSample(i))
+			self.m_featVec = []
+			for i in range(self.m_featureCount):
+				new_featVec = self.m_features[i].Eval(s.GetSample(j))
+				self.m_featVec.append(new_featVec)
+			featVec[j, :] = self.m_featVec
 
-		# featVec = np.asarray(featVec)
-		# featVec.reshape((len(s.GetRects()), 192))
 
 
 
