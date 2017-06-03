1a4d556404cd48273a4aaf12c60fa2b7c9d61e0a
Xiaofei Xian <wangjinglalala@gmail.com>
2017-06-03 16:21:54 -0700

    Fix the config file error

--- Config.py
+++ Config.py
@@ -1,85 +1,85 @@
-
 # coding: utf-8
-
-# In[1]:
-
-from enum import Enum
-import os
-
 class Feature:
+
     def __init__(self):
         self.kernelName, self.featureName, self.params = '', '', []
 
+
 class Config:
-    #FeatureName = {'kFeatureTypeHaar':'haar', 'kFeatureTypeRaw':'raw', \
+    # FeatureName = {'kFeatureTypeHaar':'haar', 'kFeatureTypeRaw':'raw',
     #               'kFeatureTypeH==togram':'h==togram'}
-    #KernelName = {'kKernelLinear':'linear', 'kKernelTypeGaussian':'gaussian', \
-    #             'kKernelTypeIntersection':'intersection', 'kKernelTypeChi2':'chi2'}
-    def __init__(self):
-        self.SetDefaults()
-    def __init__(self, path):
-        self.SetDefaults()       
-        if os.path.isfile(path) == False:
-            print 'Error: could not load config file' + path
+    # KernelName = {'kKernelLinear':'linear', 'kKernelTypeGaussian':'gaussian',
+    #             'kKernelTypeIntersection':'intersection', 'kKernelTypeChi2':'
+    #              chi2'}
+
+    def __init__(self, path=None):
+        self.setdefaults()
+        if path is None:
             return
-        f = open(path,'r')
-        for line in f:
-            words = filter(None, line.split())
-            
-            if (not words) or (len(words) < 2) or (words[0] == '#') or (words[1] != '='):
-                continue
-            if words[0] == 'seed':
-                self.seed = int(words[2])
-            elif words[0] == 'quietMode':
-                self.quietMode = words[2]
-            elif words[0] == 'debugMode':
-                self.debugMode = words[2]
-            elif words[0] == 'sequenceBasePath':
-                self.sequenceBasePath = words[2]
-            elif words[0] == 'sequenceName':
-                self.sequenceName = words[2]
-            elif words[0] == 'resultsPath':
-                self.resultsPath = words[2]
-            elif words[0] == 'frameWidth':
-                self.frameWidth = int(words[2])
-            elif words[0] == 'frameHeight':
-                self.frameHeight = int(words[2])
-            elif words[0] == 'searchRadius':
-                self.searchRadius = int(words[2])
-            elif words[0] == 'svmC':
-                self.svmC = float(words[2])
-            elif words[0] == 'svmBudgetSize':
-                self.svmBudgetSize = int(words[2])
-            elif words[0] == 'feature':
-                fkp = Feature()
-                if len(words) < 4:
-                    continue
-                if words[2] == 'haar' or words[2] == 'raw' or words[2] =='histogram':
-                    fkp.featureName = words[2]
+        try:
+            f = open(path, 'r')
+            for line in f:
+                try:
+                    words = filter(None, line.split())
+                except ValueError:
+                    print 'Error: can not accurately read config file'
                 else:
-                    print 'Error: unrecognised feature: ' + words[2]
-                    continue
-                if words[3] == 'linear' or words[3] == 'gaussian' or words[3] == 'intersection' or words[3] =='chi2':
-                    fkp.kernelName = words[3]
-                    if words[3] == 'gaussian':
-                        if len(words) == 5:
-                            fkp.params.append(words[4])
-                        else:
-                            print 'Error: unreceived param. '  
+                    if (not words) or (len(words) < 2) or (words[0] == '#') or (words[1] != '='):
+                        continue
+                    if words[0] == 'seed':
+                        self.seed = int(words[2])
+                    elif words[0] == 'quietMode':
+                        self.quietMode = words[2]
+                    elif words[0] == 'debugMode':
+                        self.debugMode = words[2]
+                    elif words[0] == 'sequenceBasePath':
+                        self.sequenceBasePath = words[2]
+                    elif words[0] == 'sequenceName':
+                        self.sequenceName = words[2]
+                    elif words[0] == 'resultsPath':
+                        self.resultsPath = words[2]
+                    elif words[0] == 'frameWidth':
+                        self.frameWidth = int(words[2])
+                    elif words[0] == 'frameHeight':
+                        self.frameHeight = int(words[2])
+                    elif words[0] == 'searchRadius':
+                        self.searchRadius = int(words[2])
+                    elif words[0] == 'svmC':
+                        self.svmC = float(words[2])
+                    elif words[0] == 'svmBudgetSize':
+                        self.svmBudgetSize = int(words[2])
+                    elif words[0] == 'feature':
+                        fkp = Feature()
+                        if len(words) < 4:
                             continue
-                else:
-                    print 'Error: unrecognised kernel: ' + words[3]
-                    continue
-                self.features.append(fkp)
-        
-    def SetDefaults(self):
+                        if words[2] == 'haar' or words[2] == 'raw' or words[2] == 'histogram':
+                            fkp.featureName = words[2]
+                        else:
+                            print 'Error: unrecognised feature: ' + words[2]
+                            continue
+                        if words[3] == 'linear' or words[3] == 'gaussian' or words[3] == 'intersection' or words[3] == 'chi2':
+                            fkp.kernelName = words[3]
+                            if words[3] == 'gaussian':
+                                if len(words) == 5:
+                                    fkp.params.append(words[4])
+                                else:
+                                    print 'Error: unreceived param. '
+                                    continue
+                        else:
+                            print 'Error: unrecognised kernel: ' + words[3]
+                            continue
+                        self.features.append(fkp)
+        except IOError as err:
+            print 'File error: ' + str(err)
+
+    def setdefaults(self):
         self.quietMode, self.debugMode = False, False
-        self.sequenceBasePath, self.sequenceName, self.resultsPath = '','',''
+        self.sequenceBasePath, self.sequenceName, self.resultsPath = '', '', ''
         self.frameWidth, self.frameHeight = 320, 240
         self.seed, self.searchRadius, self.svmC, self.svmBudgetSize = 0, 30, 1.0, 0
         self.features = []
-        
-    def PrintStr(self):
+
+    def printstr(self):
         print 'Config: '
         print '  QuietMode\t= %s' % self.quietMode
         print '  DebugMode\t= %s' % self.debugMode
@@ -98,6 +98,3 @@ class Config:
             for j in range(len(self.features[i].params)):
                 print '  ', self.features[i].params[j]
             print '\n'
-        
-    
-
