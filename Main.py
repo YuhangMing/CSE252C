423c8574c129678e498f0126090fbce930241228
Xiaofei Xian <wangjinglalala@gmail.com>
2017-06-03 20:02:22 -0700

    Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import sys
 # import getopt
 import random
 import cv2
+import linecache
 
 import numpy as np
 sys.path.append('./Rect.py')
@@ -38,14 +39,75 @@ def isFile(filePath, fileName):
         return True
 
 
+def setCamera(conf):
+    startFrame, endFrame, scaleW, scaleH, initBB = 0, MAXINT, 1.0, 1.0, Rect()
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print 'Error: the video capture do not work.'
+        return startFrame, endFrame, scaleW, scaleH, cap, initBB, False
+    ret, frame = cap.read()
+    if ret is True:
+        scaleW, scaleH = float(conf.frameWidth) / frame.shape[1], float(conf.frameHeight) / frame.shape[0]
+        initBB = Rect(int(conf.frameWidth / 2 - kLiveBoxWidth / 2), int(conf.frameHeight / 2 - kLiveBoxHeight / 2), int(kLiveBoxWidth), int(kLiveBoxHeight))
+        print 'Press "i" to initialize tracker'
+    else:
+        print 'Error: video capture invalid.'
+        return startFrame, endFrame, scaleW, scaleH, cap, initBB, False
+    return startFrame, endFrame, scaleW, scaleH, cap, initBB, True
+
+
+def setFrame(conf):
+    startFrame, endFrame, scaleW, scaleH, initBB = 0, MAXINT, 1.0, 1.0, Rect()
+
+    frameFilePath = './' + conf.sequenceBasePath + '/' + conf.sequenceName + '/frame.txt'
+    if isFile(frameFilePath, 'frame') is False:
+        return startFrame, endFrame, scaleW, scaleH, initBB, False
+    fframe = open(frameFilePath, 'r')
+    line = fframe.readline()
+    words = filter(None, line.split())
+    if len(words) != 2 or int(words[0]) < 0 or int(words[0]) > int(words[1]):
+        print 'Error: do not get the correct frame params.'    
+        return startFrame, endFrame, scaleW, scaleH, initBB, False
+    startFrame, endFrame = int(words[0]), int(words[1])
+    fframe.close()
+
+    # Get the info about the image
+    imgFormatPath = './' + conf.sequenceBasePath + '/' + conf.sequenceName + '/img/0001.jpg'
+    if isFile(imgFormatPath, 'imgFormat') is False:
+        return startFrame, endFrame, scaleW, scaleH, initBB, False
+    img = cv2.imread(imgFormatPath, 0)
+    scaleW = float(conf.frameWidth) / img.shape[1]
+    scaleH = float(conf.frameHeight) / img.shape[0]
+
+    # Get the groud truth rect
+    rectFilePath = './' + conf.sequenceBasePath + '/' + conf.sequenceName + '/groundtruth_rect.txt'
+    if isFile(rectFilePath, 'rect') is False:
+        return startFrame, endFrame, scaleW, scaleH, initBB, False
+    frect = open(rectFilePath, 'r')
+    line = frect.readline()
+    words = filter(None, line.split())
+    if len(words) != 4:
+        print 'Error: do not get the correct rect params.'
+        return startFrame, endFrame, scaleW, scaleH, False
+    xmin, ymin, width, height = float(words[0]), float(words[1]), float(words[2]), float(words[3])
+    initBB = Rect(xmin * scaleW, ymin * scaleH,width * scaleW, height * scaleH)
+    return startFrame, endFrame, scaleW, scaleH, initBB, True
+
+def getTrueRect(rectFilePath, lineid):
+    line = linecache.getline(rectFilePath, lineid)
+    words = filter(None, line.split())
+    tRect = Rect(float(words[0]), float(words[1]), float(words[2]), float(words[3]))
+    return tRect
+
+
+
+
 def main(argv=None):
     if argv is not None and len(argv) > 1:
         configPath = argv[1]
     else:
         configPath = './config.txt'
     conf = Config(configPath)
-
-    # conf.PrintStr()
 
     if len(conf.features) == 0:
         print 'Error: no features specified in config'
@@ -56,79 +118,29 @@ def main(argv=None):
     fres = open(conf.resultsPath, 'w')
 
     useCamera = (conf.sequenceName == '')
-    startFrame, endFrame, scaleW, scaleH = -1, -1, 1.0, 1.0
 
     if useCamera:
-        # Do the camera traning
-        cap = cv2.VideoCapture(0)
-        if not cap.isOpened():
-            print 'Error: the video capture do not work.'
-            return 0
-        startFrame, endFrame = 0, MAXINT
-        ret, frame = cap.read()
-        if ret is True:
-            scaleW, scaleH = float(
-                conf.frameWidth) / frame.shape[1], float(conf.frameHeight) / frame.shape[0]
-            initBB = Rect(int(conf.frameWidth / 2 - kLiveBoxWidth / 2), int(
-                conf.frameHeight / 2 - kLiveBoxHeight / 2), int(kLiveBoxWidth), int(kLiveBoxHeight))
-            print 'Press "i" to initialize tracker'
-        else:
-            print 'Error: video capture invalid.'
-            return 0
+        startFrame, endFrame, scaleW, scaleH, cap, initBB, success = setCamera(conf)
     else:
-        # Do the sequence training.
-        # Get the start and end frame
-        frameFilePath = './' + conf.sequenceBasePath + \
-            '/' + conf.sequenceName + '/frame.txt'
-        if isFile(frameFilePath, 'frame') is False:
-            return 0
-        fframe = open(frameFilePath, 'r')
-        line = fframe.readline()
-        words = filter(None, line.split())
-        if len(words) != 2 or int(words[0]) < 0 or int(words[0]) > int(words[1]):
-            print 'Error: do not get the correct frame params.'
-            return 0
-        startFrame, endFrame = int(words[0]), int(words[1])
-
-        # Get the info about the image
-        imgFormatPath = './' + conf.sequenceBasePath + \
-            '/' + conf.sequenceName + '/img/0001.jpg'
-        if isFile(imgFormatPath, 'imgFormat') is False:
-            return 0
-        img = cv2.imread(imgFormatPath, 0)
-        scaleW = float(conf.frameWidth) / img.shape[1]
-        scaleH = float(conf.frameHeight) / img.shape[0]
-
-        # Get the groud truth rect
-        rectFilePath = './' + conf.sequenceBasePath + '/' + \
-            conf.sequenceName + '/groundtruth_rect.txt'
-        if isFile(rectFilePath, 'rect') is False:
-            return 0
-        frect = open(rectFilePath, 'r')
-        line = frect.readline()
-        words = filter(None, line.split())
-        if len(words) != 4:
-            print 'Error: do not get the correct rect params.'
-            return 0
-        xmin, ymin, width, height = float(words[0]), float(
-            words[1]), float(words[2]), float(words[3])
-        initBB = Rect(xmin * scaleW, ymin * scaleH,
-                      width * scaleW, height * scaleH)
+        startFrame, endFrame, scaleW, scaleH, initBB, success = setFrame(conf)
+    if not success:
+        return 0
 
     tracker = Tracker(conf)
     if not conf.quietMode:
         cv2.namedWindow('Result')
 
-    # result = cv.createMat(conf.frameHeight, conf.frameWidth, cv.CV_8UC3)
     result = np.zeros((conf.frameHeight, conf.frameWidth, 3), np.uint8)
-    paused, doInitialize = False, False
+    paused, doInitialize = True, False
     random.seed(conf.seed)
+    count, totalOverlap = 0, 0.0
+    rectFilePath = './' + conf.sequenceBasePath + '/' + conf.sequenceName + '/groundtruth_rect.txt'
+
     for frameid in range(startFrame, endFrame + 1):
         if useCamera:
             ret, frameOrig = cap.read()
             if ret is True:
-                frame = cv2.resize(
-                    frameOrig, (conf.frameWidth, conf.frameHeight))
+                frame = cv2.resize(frameOrig, (conf.frameWidth, conf.frameHeight))
                 frame = cv2.flip(frame, 1)
                 result = frame[:]
                 if doInitialize:
@@ -141,18 +153,16 @@ def main(argv=None):
                     if not tracker.IsInitialized():
                         rectangle(result, initBB, WHITE)
             else:
-                print 'Error: do not capture valid video.'
+                print 'Error: video capture invalid.'
                 return 0
         else:
-
-            imgFramePath = './' + conf.sequenceBasePath + '/' + \
-                conf.sequenceName + ('/img/%04d.jpg' % int(frameid))
+            imgFramePath = './' + conf.sequenceBasePath + '/' + conf.sequenceName + ('/img/%04d.jpg' % int(frameid))
             if isFile(imgFramePath, 'imgFrame') is False:
-                return 0
+                return False
             frameOrig = cv2.imread(imgFramePath, 0)
             if frameOrig is None:
                 print 'Error: do not get valid input frame image.'
-                return 0
+                return False
             frame = cv2.resize(frameOrig, (conf.frameWidth, conf.frameHeight))
             result = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
@@ -161,42 +171,55 @@ def main(argv=None):
 
         if tracker.IsInitialized():
             tracker.Track(frame)
-
-            # incomplete tracker
             # if !conf.quietMode and conf.debugMode:
             #     tracker.Debug()
-
             rectangle(result, tracker.GetBox(), GREEN)
-
             box = tracker.GetBox()
-            fres.write('%d %d %d %d\n' % (box.XMin() / scaleW, box.YMin() /
-                                          scaleH, box.Width() / scaleW, box.Height() / scaleH))
+            fRect = Rect(box.XMin() / scaleW, box.YMin() / scaleH, box.Width() / scaleW, box.Height() / scaleH)
+            fres.write('%d %d %d %d' % (fRect.XMin(), fRect.YMin(), fRect.Width(), fRect.Height()))
+            if not useCamera:
+                tRect = getTrueRect(rectFilePath, frameid)
+                overlap = float(tRect.Overlap(fRect))
+                print 'true Rect: ',
+                tRect.printStr()
+                print 'tracker Rect: ',
+                box.printStr()
+                print 'overlap: %.2f' % overlap
+                count, totalOverlap = count + 1, totalOverlap + overlap
+                fres.write(' %.2f' % overlap)
+            fres.write('\n')
+
+        print "hello image"
 
         if not conf.quietMode:
+            print 'show image'
             cv2.imshow("result", result)
-            key = cv2.waitKey(0 if paused else 1)
+            if paused:
+                key = cv2.waitKey() & 0xFF
+            else:
+                key = cv2.waitKey(1) & 0xFF
             if key != -1:
-                if key == 27 or key == 113:
+                if key == 27 or key == 113: # esc q
                     break
-                elif key == 112:
+                elif key == 112: # p
                     paused = not paused
-                elif key == 105 and useCamera:
+                elif key == 105 and useCamera: # i
                     doInitialize = True
+                    print 'doInitialize'
             if conf.debugMode and frameid == endFrame:
                 print 'End of sequence, press any key to exit.'
                 cv2.waitKey()
 
-    if (useCamera):
+
+    if useCamera:
         cap.release()
+    else:
+        score = totalOverlap / float(count)
+        fres.write('%.3f\n' % score)
     if not fres.closed:
         fres.close()
-    if not frame.closed:
-        frame.close()
-    if not frect.closed:
-        frect.close()
 
     cv2.destroyAllWindows()
-    
     return 1
 
 
