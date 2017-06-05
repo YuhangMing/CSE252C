c53271427e6bcbf1e465ce192e02030587fcb483
Xiaofei Xian <wangjinglalala@gmail.com>
2017-06-05 01:45:44 -0700

    Multikernel complete

--- Kernels.py
+++ Kernels.py
@@ -8,22 +8,27 @@ import numpy as np
 class Kernel:
     def __init__(self):
         pass
+
     def Eval(self, x1, x2=None):
         pass
-    
+
+
 class LinearKernel(Kernel):
     def __init__(self):
-        pass
+        Kernel.__init__(self)
+
     def Eval(self, x1, x2=None):
         if x2 != None:
             return np.dot(x1, x2)
         else:
             return np.linalg.norm(x1)
 
+
 class GaussianKernel(Kernel):
     def __init__(self, sigma):
         Kernel.__init__(self)
         self.m_sigma = sigma
+
     def Eval(self, x1, x2=None):
         if x2 is not None:
             norm_val = (np.linalg.norm(x1-x2)) ** 2
@@ -36,32 +41,51 @@ class GaussianKernel(Kernel):
             # return np.exp(np.linalg.norm(inner))
         else:
             return 1.0
-    
+
+
 class IntersectionKernel(Kernel):
     def __init__(self):
-        pass
+        Kernel.__init__(self)
+
     def Eval(self, x1, x2=None):
         if x2 is not None:
             return np.minimum(x1, x2).sum()
         else:
             return np.sum(x1)
 
-#incomplete
+
+#complete
 class Chi2Kernel(Kernel):
     def __init__(self):
-        pass
-    def Eval(self, x1, x2=None):
-        pass
+        Kernel.__init__(self)
 
-#incomplete    
+    def Eval(self, x1, x2=None):
+        if x2 is not None:
+            result = 0.0
+            for i in range(len(x1)):
+                a, b = x1[i], x2[i]
+                result += (a-b)*(a-b)/(0.5*(a+b)+1e-8)
+            return 1.0 - result
+        else:
+            return 1.0
+
+
+
+#complete    
 class MultiKernel(Kernel):
     def __init__(self, kernels, featureCounts):
         Kernel.__init__(self)
-        self.m_n, self.m_norm, self.m_kernels, self.m_counts = kernels.size(), 1.0/kernels.size(), kernels, featureCounts
-    def Eval(self, x1, x2):
+        self.m_n, self.m_norm, self.m_kernels, self.m_counts = len(kernels), 1.0/len(kernels), kernels, featureCounts
+
+    def Eval(self, x1, x2=None):
         total, start = 0.0, 0.0
         for i in range(self.m_n):
             c = self.m_counts[i]
-            #total += self.m_norm * self.m_kenerls[i].Eval()
-        
+            if x2 is not None:
+                total += self.m_norm * self.m_kenerls[i].Eval(x1[start:start+c], x2[start:start+c])
+            else:
+                total += self.m_norm * self.m_kenerls[i].Eval(x1[start:start+c])
+            start+=c
+        return total
+
 
