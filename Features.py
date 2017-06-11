a7c6b762772d68a770ee9a0b054eefb6b72c326a
Xiaofei Xian <wangjinglalala@gmail.com>
2017-06-11 02:13:17 -0700

    add features.py

---
+++ Features.py
@@ -0,0 +1,27 @@
+import sys
+import cv2 as cv
+import numpy as np
+sys.path.append('./Sample.py')
+from Sample import Sample
+
+class Feature:
+    def __init__(self):
+    	self.m_featureCount = 0
+
+    def SetCount(self, c):
+    	self.m_featureCount = c
+    	self.m_feat = []
+
+    def GetCount(self):
+    	return self.m_featureCount
+
+    def UpdateFeature(self, sam):
+    	pass
+
+    def Eval(self, multisam, feats=None):
+    	if feats is None:
+    		self.UpdateFeature(multisam)
+    		return self.m_feat
+    	else:
+    		for i in range(len(multisam.GetRects())):
+    			self.m_feat.append(Eval(multisam.GetSample(i)))
