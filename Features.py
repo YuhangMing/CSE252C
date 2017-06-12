748e0716f8ed4c4211881851d91722fd5b5dcb59
Xiaofei Xian <wangjinglalala@gmail.com>
2017-06-11 17:02:32 -0700

    edit in Eval

--- Features.py
+++ Features.py
@@ -27,5 +27,5 @@ class Features:
         else:
             # ???
             featVecs = []
-            for i in (len(featVecs)):
+            for i in (len(s.GetRects())):
                 featVecs.append(self.Eval(s.GetSample(i)))
